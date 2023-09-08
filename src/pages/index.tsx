6a10919cbad80f0845235c2b9a33ccd1f504a217
mabdulkarma123 <cjzchong1@gmail.com>
2023-09-08 17:35:17 +0800

    Fix locale warning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import HeroSection from './../app/components/Index/HeroSection';
-import FeaturesSection from './../app/components/Index/FeaturesSection';
-import WeDoSection from './../app/components/Index/WeDoSection';
-
+import { GetStaticPropsContext } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import HeroSection from '@/app/components/Index/HeroSection';
 import ServicesSection from '@/app/components/Index/ServiceSection';
 import AboutUsSection from '@/app/components/Index/AboutSection';
 import Contact from '@/app/components/Contact/Contact';
 
 const Home: React.FC = () => {
-  // const { t } = useTranslation();
+
   const { t } = useTranslation('common');
-  // console.log(t('test'),'tt');
   
   return (
     <div>
       <HeroSection />
       <AboutUsSection/>
-      {/* <FeaturesSection /> */}
-      {/* <WeDoSection /> */}
       <ServicesSection />
       <Contact/>
     </div>
@@ -28,10 +24,11 @@ const Home: React.FC = () => {
 
 export default Home;
 
-export async function getStaticProps({locale}) {
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  const actualLocale = locale || 'en'; 
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common"])),
+      ...(await serverSideTranslations(actualLocale, ['common'])),
     },
-  }
-};
+  };
+}
